fad8d87a8dbb763c484fa1782bfaee31816d4bac
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2021-08-01 00:15:08 +0300

    рендеринг
    Добавлен рендеринг страницы

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import{ msg } from './js/message.js';
+const ObjJSON = require('./menu.json');
+import templateFunction from './templates/template.hbs';
+
+const markup = ObjJSON.map(item => templateFunction(item)).join('');
+
+const containerEl = document.querySelector('.js-menu');
+containerEl.insertAdjacentHTML('afterbegin', markup);
--- src/js/message.js
+++
@@ -1,1 +0,0 @@
-export const msg = console.log('hello world!');
