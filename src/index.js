e6f9a22e29e8ae5134a5634e6494d8a8931dd6ad
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2021-08-01 08:12:06 +0300

    ф-л темы

--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
-const ObjJSON = require('./menu.json');
 import templateFunction from './templates/template.hbs';
+const arrData = require('./menu.json');
 
-const markup = ObjJSON.map(item => templateFunction(item)).join('');
+// Рендеринг
+const markup = arrData.map(item => templateFunction(item)).join('');
 
 const containerEl = document.querySelector('.js-menu');
 containerEl.insertAdjacentHTML('afterbegin', markup);
+
+// Функционал выбора темы
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+};
+
+const themeSwitchEl = document.querySelector('.theme-switch__toggle');
+const bodyEl = document.querySelector('body');
+
+themeSwitchEl.addEventListener('change', onChangeTheme);
+
+selectTheme();
+
+function selectTheme() {
+    const theme = localStorage.getItem('theme');
+
+    if (theme === Theme.DARK) {
+        themeSwitchEl.checked = true;
+        bodyEl.classList.remove(Theme.LIGHT);
+        bodyEl.classList.add(Theme.DARK);
+    } else {
+        themeSwitchEl.checked = false;
+        bodyEl.classList.remove(Theme.DARK);
+        bodyEl.classList.add(Theme.LIGHT);
+    }
+}
+
+function onChangeTheme() {
+    if (themeSwitchEl.checked) {
+        localStorage.setItem('theme', Theme.DARK);
+    } else {
+        localStorage.setItem('theme', Theme.LIGHT);
+    }
+    selectTheme();
+}
